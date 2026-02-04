036f91a68c89ecf6e4996bc5405b9cf0704d2d7f
ddheerajc64 <dheerajgithub64@gmail.com>
2026-02-04 10:42:55 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,43 @@
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.open("study-hub").then(cache=>{
-      return cache.match(e.request).then(r=>{
-        return r || fetch(e.request).then(res=>{
-          cache.put(e.request, res.clone());
-          return res;
-        });
-      });
-    })
-  );
+const CACHE_NAME = 'studyhub-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+    // Add specific unit HTML files here if they exist physically, 
+    // e.g., './unit1.html', etc.
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS);
+            })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then((response) => {
+                return response || fetch(e.request);
+            })
+    );
 });
