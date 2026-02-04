e2dab3c79233f55877b2039a891dff56de479aa7
ddheerajc64 <dheerajgithub64@gmail.com>
2026-02-04 10:37:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,9 @@
-self.addEventListener("fetch", e=>{
+self.addEventListener("fetch",e=>{
   e.respondWith(
-    caches.open("study-hub").then(cache=>{
-      return cache.match(e.request).then(r=>{
-        return r || fetch(e.request).then(res=>{
-          cache.put(e.request,res.clone());
-          return res;
-        });
-      });
-    })
+    caches.open("study-hub").then(c=>
+      c.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+        c.put(e.request,res.clone());return res;
+      }))
+    )
   );
 });
