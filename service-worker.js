1bb4a3de66e30ece2eb103184d97b03649131512
ddheerajc64 <dheerajgithub64@gmail.com>
2026-02-04 10:36:47 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.open("study-hub").then(cache=>{
+      return cache.match(e.request).then(r=>{
+        return r || fetch(e.request).then(res=>{
+          cache.put(e.request,res.clone());
+          return res;
+        });
+      });
+    })
+  );
+});
